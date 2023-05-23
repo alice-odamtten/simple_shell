07f590c95e16254f91bc8cd3ca5d5a890cc5f7f5
DD1C74707 <gracevalley101@gmail.com>
2023-05-23 05:55:00 -0700

    Add read line function

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
  * main - entry point
  * @c:  number of cli arguments
  * @av: an array of cli arguments
- * @emv: environment variables
+ * @env: environment variables
  * Return: 1
  */
 int main(int c, char **av, char **env)
@@ -16,3 +16,29 @@ int main(int c, char **av, char **env)
 
 	return (1);
 }
+
+/**
+ * sh_read_line - reads inputs from cli
+ * @info: global data for state management
+ * Return: input from the cli
+ */
+char *sh_read_line(g_data *info)
+{
+	char *line = NULL;
+	static size_t buflen;
+
+	if (_getline(&line, buflen, info->readfd) == -1)
+	{
+		if (feof(stdin))
+		{
+			perror("getline");
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			exit(EXIT_FAILURE);
+		}
+	}
+	return (line);
+}
+
